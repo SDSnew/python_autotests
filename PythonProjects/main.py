8bc5897619f3416e510b4bc7ddf22c43cf1d8366
SDSnew <Shipoff-danil@mail.ru>
2025-08-11 15:48:06 +0500

    Add files via upload

--- PythonProjects/main.py
+++ PythonProjects/main.py
@@ -1,22 +1,26 @@
 import requests
 
 URL = "https://api.pokemonbattle.ru/v2"
-TOKEN = "USER_TOKEN"
+TOKEN = "Trainer_token"
 HEADER = {"Content-Type": "application/json", "trainer_token": TOKEN}
 TRAINER_ID = "37588"
 json_create_pokemon = {"name": "Данил", "photo_id": 12}
 
-json_change_name = {"pokemon_id": "372802", "name": "Пикачу", "photo_id": 2}
-
-json_add_to_pokebol = {"pokemon_id": "372802"}
 
 response = requests.post(
     url=f"{URL}/pokemons", headers=HEADER, json=json_create_pokemon
 )
 print(response.json())
 
+pokemon_id = response.json()["id"]
+
+json_change_name = {"pokemon_id": pokemon_id, "name": "Пикачу", "photo_id": 2}
+json_add_to_pokebol = {"pokemon_id": pokemon_id}
+
+
 response = requests.put(url=f"{URL}/pokemons", headers=HEADER, json=json_change_name)
 print(response.json())
+
 
 response = requests.post(
     url=f"{URL}/trainers/add_pokeball", headers=HEADER, json=json_add_to_pokebol
